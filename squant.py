63a170fef2f21bbe374ddefc28840d113ba7060f
EmmanuelOgbewe <emmanuel.ogbewe12@gmail.com>
2020-04-18 23:55:07 -0400

    First Commit

---
+++ squant.py
@@ -0,0 +1,20 @@
+# squant.py 
+#@author Emmanuel Ogbewe 
+
+import sys
+import gzip
+
+#------Handle input------#
+def parseCommandLineArgs(args=[]):
+    print(args)
+   
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Running")
+    parseCommandLineArgs(sys.argv)
